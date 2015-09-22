a9da7734454d60d2393dd65c78edfc5a230729c6
usuario <usuario@10.22.143.90>
2015-09-22 09:46:32 +0100

    primera version

---
+++ src/kata/KATA.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata;
+
+import java.util.Date;
+
+/**
+ *
+ * @author usuario
+ */
+public class KATA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+
+     //   Person p1;
+     //   Date f;
+     //   f = new Date(64, 7, 3);
+
+
+       // p1 = new Person("Enrique", "Ramon", f);
+       // System.out.println(p1.getFullName());
+       // System.out.println(p1.getBirthdyy());
+       // System.out.println(p1.getAge());
+        System.out.println("Fin");
+
+    }
+}
